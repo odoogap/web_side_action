b6af2c0347031361effd5c62d8850932995d557d
Vasantrav Kavthekar <vasantravk@odoogap.com>
2019-01-08 14:00:18 +0530

    Normalize code, create mixins for renderers and controllers to use in other views

--- web_side_action/static/src/js/side_action.js
+++ web_side_action/static/src/js/side_action.js
@@ -13,128 +13,99 @@ odoo.define('web.sideaction',function(require){
 	var FormController = require('web.FormController');
 	var FormRenderer = require('web.FormRenderer');
 
-	var get_action_details = function(self, action_data){
-		console.log("self ", self, action_data);
-    	return self._rpc({
-            model: action_data.type,
-            method: 'read',
-            args: [action_data.id, ["type","name","tag","res_model","views","view_type","view_mode","target","context","domain"]],
-        });
-	}
-
-	var get_action_type = function(self, action_id){
-		console.log("self ", self);
-    	return self._rpc({
-            model: 'ir.actions.actions',
-            method: 'read',
-            args: [action_id, ["type","name"]],
-        });
-	}
-
-	var get_action_id = function(self, xmlid){
-		return self._rpc({
-            model: 'ir.model.data',
-            method: 'xmlid_to_res_id',
-            kwargs: {xmlid: xmlid},
-        });
-	}
-
-	var setup_extra_buttons = function(target){
-		var self = target;
-		if(!self.side_action){
-			return;
-		}
-    	get_action_id(self, self.side_action).then(function(action_id){
-    		get_action_type(self, action_id).then(function(action_data){
-    			var action_data = action_data[0]
-	    		self.side_action_name = action_data.name;
-				$(document).find("[data-action='" + self.side_action + "']").html(self.side_action_name);
-
-    			get_action_details(self, action_data).then(function (finalresult) {
-    				self.sideactions[self.side_action] = finalresult[0];
-        			self.$buttons.on('click', '.o_list_button_extra', self._clickExtraButtons.bind(self));
-        		});
-    		})
-    	});
-	}
-
-	ListRenderer.include({
-	    init: function (parent, state, params) {
-        	this._super.apply(this, arguments);
+	var ControllerMixin = {
+	    custom_init: function (parent, state, params) {
         	this.side_action = this.arch.attrs.side_action;
-        	console.log("this.side_action ", this.side_action);
 	    },
-	});
-
-	ListController.include({
-		renderButtons: function ($node) {
+	    _clickExtraButtons: function (event) {
+	        event.stopPropagation();
+	        var action_data = this.sideactions[$(event.target).data('action')];
+	        this.do_action(action_data);
+	    },
+		get_action_details: function(action_data){
+			return this._rpc({
+	            model: action_data.type,
+	            method: 'read',
+	            args: [action_data.id, ["type","name","tag","res_model","views","view_type","view_mode","target","context","domain"]],
+	        });
+		},
+		get_action_type: function(action_id){
+			return this._rpc({
+	            model: 'ir.actions.actions',
+	            method: 'read',
+	            args: [action_id, ["type","name"]],
+	        });
+		},
+		get_action: function(xmlid){
+			return this._rpc({
+	            model: 'ir.model.data',
+	            method: 'xmlid_to_res_id',
+	            kwargs: {xmlid: xmlid},
+	        });
+		},
+		setup_extra_buttons: function(){
 			var self = this;
-            self.side_action = self.renderer.side_action;
+			if(!self.side_action){
+				return;
+			}
+	    	self.get_action(self.side_action).then(function(action_id){
+	    		self.get_action_type(action_id).then(function(action_data){
+	    			var action_data = action_data[0]
+		    		self.side_action_name = action_data.name;
+					$(document).find("[data-action='" + self.side_action + "']").html(self.side_action_name);
+
+	    			self.get_action_details(action_data).then(function (finalresult) {
+	    				self.sideactions[self.side_action] = finalresult[0];
+	        			self.$buttons.on('click', '.o_list_button_extra', self._clickExtraButtons.bind(self));
+	        		});
+	    		})
+	    	});
+		},
+	    _bindButtons: function () {
+	    	var self = this;
+            self.side_action = this.renderer.side_action;
         	self.side_action_name = '';
         	self.sideactions = {}
-    		setup_extra_buttons(self);
-			self._super.apply(this, arguments);
+    		self.setup_extra_buttons();
+	    },
+	};
+
+	var CommonRendererMixin = {
+	    init: function (parent, state, params) {
+        	this._super.apply(this, arguments);
+        	ControllerMixin.custom_init.apply(this, arguments);
+	    },
+	};
+
+	var CommonControllerMixin = {
+		get_action_details: function(action_data){
+        	return ControllerMixin.get_action_details.apply(this, arguments);
+		},
+		get_action_type: function(action_id){
+        	return ControllerMixin.get_action_type.apply(this, arguments);
+		},
+		get_action: function(xmlid){
+        	return ControllerMixin.get_action.apply(this, arguments);
+		},
+		setup_extra_buttons: function(){
+        	ControllerMixin.setup_extra_buttons.apply(this, arguments);
+		},
+		renderButtons: function ($node) {
+        	ControllerMixin._bindButtons.call(this);
+			this._super.apply(this, arguments);
 		},
 	    _clickExtraButtons: function (event) {
-	        event.stopPropagation();
-	        var action_data = this.sideactions[$(event.target).data('action')];
-	        this.do_action(action_data);
+        	ControllerMixin._clickExtraButtons.apply(this, arguments);
 	    },
-	});
+	};
 
-	KanbanRenderer.include({
-	    init: function (parent, state, params) {
-        	this._super.apply(this, arguments);
-        	this.side_action = this.arch.attrs.side_action;
-	    },
-	});
-	
-	KanbanController.include({
-	    init: function () {
-	        this._super.apply(this, arguments);
-	    },
-	    renderButtons: function () {
-	    	var self = this;
-            self.side_action = self.renderer.side_action;
-        	self.side_action_name = '';
-        	self.sideactions = {}
+	ListRenderer.include(CommonRendererMixin);
+	ListController.include(CommonControllerMixin);
 
-	        this._super.apply(this, arguments);
-        	setup_extra_buttons(this);
-	    },
-	    _clickExtraButtons: function (event) {
-	        event.stopPropagation();
-	        var action_data = this.sideactions[$(event.target).data('action')];
-	        this.do_action(action_data);
-	    },
-	});
+	KanbanRenderer.include(CommonRendererMixin);
+	KanbanController.include(CommonControllerMixin);
 
-	FormRenderer.include({
-	    init: function (parent, state, params) {
-        	this._super.apply(this, arguments);
-        	this.side_action = this.arch.attrs.side_action;
-        	console.log("form this.side_action ", this.side_action);
-	    },
-	});
-	
-	FormController.include({
-	    init: function () {
-	        this._super.apply(this, arguments);
-	    },
-	    renderButtons: function () {
-	    	var self = this;
-            self.side_action = self.renderer.side_action;
-        	self.side_action_name = '';
-        	self.sideactions = {}
-
-	        this._super.apply(this, arguments);
-        	setup_extra_buttons(this);
-	    },
-	    _clickExtraButtons: function (event) {
-	        event.stopPropagation();
-	        var action_data = this.sideactions[$(event.target).data('action')];
-	        this.do_action(action_data);
-	    },
-	});
+	FormRenderer.include(CommonRendererMixin);
+	FormController.include(CommonControllerMixin);
 
 });
